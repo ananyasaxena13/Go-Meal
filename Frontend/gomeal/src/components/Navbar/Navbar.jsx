a035e279f3891fc7cda57a186f91dcf9bf11be57
AS-Dontella <96739733+AS-Dontella@users.noreply.github.com>
2024-07-09 00:49:01 +0530

    frontend

--- Frontend/gomeal/src/components/Navbar/Navbar.jsx
+++ Frontend/gomeal/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -10,9 +11,9 @@ const Navbar = () => {
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li onClick={()=>setMenu("Home")}className={menu === "Home"?"active":""} >Home</li>
-        <li onClick={()=>setMenu("Menu")}className={menu === "Menu"?"active":""} >Menu</li>
-        <li onClick={()=>setMenu("ContactUs")}className={menu === "ContactUs"?"active":""} >Contact Us</li>
+        <Link to='/' onClick={()=>setMenu("Home")}className={menu === "Home"?"active":""} >Home</Link>
+        <a href='#explore-menu' onClick={()=>setMenu("Menu")}className={menu === "Menu"?"active":""} >Menu</a>
+        <a href='#footer' onClick={()=>setMenu("ContactUs")}className={menu === "ContactUs"?"active":""} >Contact Us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
