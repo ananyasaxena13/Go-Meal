dcb06f06daf596b84cd3ccde3550468ee81d3605
AS-Dontella <96739733+AS-Dontella@users.noreply.github.com>
2024-07-22 17:42:25 +0530

    admin

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         <img className='logo' src={assets.logo} alt="" />
-        <img clssrc="" alt="" />
+        <img className='profile' src={assets.profile_image} alt="" />
       
     </div>
   )
