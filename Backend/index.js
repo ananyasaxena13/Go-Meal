6788339c8de0e18dfb5113309f3c8ce42bca3583
AS-Dontella <96739733+AS-Dontella@users.noreply.github.com>
2024-07-23 15:46:20 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -2,31 +2,36 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config.js'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
-
-
+//app config
 const app = express()
-const port=4000
+const port = 4000
 
-//using this middleware the request from frontend to backend will parse through json
+//middleware
 app.use(express.json())
 app.use(cors())
 
-// db connection
+
+//db connection
 connectDB();
 
-// api endpoints
+//api endpoints
 app.use("/api/food",foodRouter)
-app.use("/images",express.static("uploads"))
-
+app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 
 app.get("/",(req,res)=>{
-    res.send("server is running")
+    res.send("API Working")
+
 })
 
 app.listen(port,()=>{
-    console.log(`server is running on http://localhost:${port}`)
+    console.log(`server started on http://localhost:${port}`)
 })
-
-
